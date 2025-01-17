0defcde0ba206acf11862c4d284750a199e44452
vedant-io <vedantd004@gmail.com>
2025-01-17 22:34:53 +0530

    Some Changes made to backend and added Socket.io functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,50 +1,49 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import { generateToken } from "../libs/utils.route.js";
-import cloudinary from "../libs/cloudinary.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
-  const { email, fullName, password, profilePic } = req.body;
+  const { fullName, email, password } = req.body;
   try {
-    //Hash Password
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
     if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ error: "Password is less than 6 characters" });
+      return res.status(400).json({ message: "Password must be at least 6 characters" });
     }
 
     const user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ error: "User already exists" });
-    }
+
+    if (user) return res.status(400).json({ message: "Email already exists" });
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
-      email,
       fullName,
+      email,
       password: hashedPassword,
     });
-    console.log(res)
 
     if (newUser) {
-      //generate jwt token
+      // generate jwt token here
       generateToken(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({
         _id: newUser._id,
-        email: newUser.email,
         fullName: newUser.fullName,
+        email: newUser.email,
         profilePic: newUser.profilePic,
       });
     } else {
-      return res.status(400).json({ error: "Invalid User Data Entered" });
+      res.status(400).json({ message: "Invalid user data" });
     }
-  } catch (err) {
-    console.log("Error for New User/signup controller :  " + err);
-    return res.status(400).json({ error: "Internal Server Error" });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
@@ -54,61 +53,66 @@ export const login = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ error: "Invalid Credentials" });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
     if (!isPasswordCorrect) {
-      return res.status(400).json({ error: "Invalid Credentials" });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     generateToken(user._id, res);
 
     res.status(200).json({
       _id: user._id,
-      email: user.email,
       fullName: user.fullName,
+      email: user.email,
       profilePic: user.profilePic,
     });
   } catch (error) {
-    console.log("Error for Login controller :  " + error);
-    return res.status(500).json({ error: "Internal Server Error" });
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 export const logout = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
-    return res.status(200).json({ message: "User Logged Out Successfully" });
-  } catch (error) {}
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const updateProfile = async (req, res) => {
-  try{
-    const {profilePic} = req.body;
+  try {
+    const { profilePic } = req.body;
     const userId = req.user._id;
 
     if (!profilePic) {
-      return res.status(400).json({ error: "ProfilePic is required" });
+      return res.status(400).json({ message: "Profile pic is required" });
     }
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
-    const updatedUser = await User.findById(userId, {profilePic: uploadResponse.secure_url},{new:true})
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
 
-    return res.status(200).json({updatedUser})
-  }catch(err){
-    console.log("Error in update profile pic of user " + err);
-    return res.status(500).json({ error: "Internal Server Error" });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const checkAuth = (req, res) => {
-  try{
+  try {
     res.status(200).json(req.user);
-
-  }catch(err){
-    res.status(500).json({ error: "Internal Server Error" });
-    console.log("Error from checkAuth" + err);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,20 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
-import { connectDb } from "./libs/db.js";
 import cookieParser from "cookie-parser";
-import messageRoute from "./routes/message.route.js";
 import cors from "cors";
 
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
-const app = express();
-const port = process.env.PORT || 3000;
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,13 +22,21 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  }),
+  })
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoute);
+app.use("/api/messages", messageRoutes);
 
-app.listen(port, () => {
-  console.log("Server is running on port " + port);
-  connectDb();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
