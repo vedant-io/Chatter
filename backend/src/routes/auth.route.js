0efd0f53746b7e7a33b34f3aec6c5901a9195f33
vedant-io <vedantd004@gmail.com>
2025-01-17 22:34:53 +0530

    Some Changes made to backend, added frontend and complete uifunctionality and added Socket.io functionality

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,24 +1,11 @@
 import express from "express";
-import {
-  checkAuth,
-  login,
-  logout,
-  signup,
-  updateProfile,
-} from "../controllers/auth.controller.js";
-import {
-  loginSchema,
-  signupSchema,
-  validateRequest,
-} from "../libs/validator.js";
-import { protectRoute } from "../middleware/protectRoute.middleware.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", validateRequest(signupSchema), signup);
-
-router.post("/login", validateRequest(loginSchema), login);
-
+router.post("/signup", signup);
+router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
