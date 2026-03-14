bedd97ba33e79f38c5160fc2350464ab39169d4a
vedant-io <vedantd004@gmail.com>
2026-03-15 05:18:41 +0530

    Test

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 const PORT = process.env.PORT || 5000;
-
+//test
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "2mb" }));
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
